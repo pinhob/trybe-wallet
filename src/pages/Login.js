4487d7754dd665b2fd2fd371f4efdea617ad4524
Bruno Pinho <brunopinho@outlook.com>
2021-09-14 11:47:48 -0300

    feito requisito 1 e estrutura da store

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          type="email"
+          name="email-input"
+          placeholder="Email"
+          id="email-input"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          name="password-input"
+          placeholder="Senha"
+          id="password-input"
+          data-testid="password-input"
+        />
+        <button type="submit">
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
